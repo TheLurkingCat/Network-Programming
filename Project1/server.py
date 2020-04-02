8dfd29ddb260d131c87ff974cb7a481fda1c70f1
P.T.Liu <32132616+TheLurkingCat@users.noreply.github.com>
2020-04-02 14:08:41 +0800

    Change to StreamHandler

--- Project1/server.py
+++ Project1/server.py
@@ -1,86 +1,112 @@
 #!/usr/bin/python3
+import re
+import socket
 import socketserver
 import sys
 
 import pymongo
 
-WELCOME = "********************************\n\
-** Welcome to the BBS server. **\n\
-********************************\n".encode()
+WELCOME = [b"********************************\r\n",
+           b"** Welcome to the BBS server. **\r\n",
+           b"********************************\r\n"]
 
-PROMPT = "% ".encode()
+PROMPT = b"% "
 
-HELP_REG = "Usage: register <username> <email> <password>\n".encode()
-HELP_LOGIN = "Usage: login <username> <password>\n".encode()
-FAIL_REG = "Username is already used.\n".encode()
-FAIL_LOGIN_ALREADY = "Please logout first.\n".encode()
-FAIL_LOGIN_INCORRECT = "Login failed.\n".encode()
-FAIL_LOGOUT_UNAUTH = "Please login first.\n".encode()
-FAIL_UNAUTHORIZED = "Please login first.\n".encode()
+HELP_REG = b"Usage: register <username> <email> <password>\r\n"
+HELP_LOGIN = b"Usage: login <username> <password>\r\n"
+FAIL_REG = b"Username is already used.\r\n"
+FAIL_LOGIN_ALREADY = b"Please logout first.\r\n"
+FAIL_LOGIN_INCORRECT = b"Login failed.\r\n"
+FAIL_LOGOUT_UNAUTH = b"Please login first.\r\n"
+FAIL_UNAUTHORIZED = b"Please login first.\r\n"
+APPLY_BACKSPACE = re.compile('[^\x08]\x08')
+REMOVE_TRAILING_BACKSPACE = re.compile('\x08+')
 
 
-class Server(socketserver.BaseRequestHandler):
+def apply_backspace(string):
+    while True:
+        temp_string = APPLY_BACKSPACE.sub('', string)
+        if len(string) == len(temp_string):
+            return REMOVE_TRAILING_BACKSPACE.sub('', temp_string)
+        string = temp_string
+
+
+class Server(socketserver.StreamRequestHandler):
     def handle(self):
         print("New connection.")
-        print(self.client_address)
-        self.request.sendall(WELCOME)
-        client = pymongo.MongoClient()['NP']['user']
-        authorized = False
+        print(self.client_address, "start the connection.")
+        self.wfile.writelines(WELCOME)
+        client = pymongo.MongoClient()
+        collection = client['NP']['user']
         name = None
         while True:
-            self.request.sendall(PROMPT)
-            recv_data = self.request.recv(4096)
+            self.wfile.write(PROMPT)
+            recv_data = self.rfile.readline()
             if not recv_data:
                 break
             try:
-                commands = recv_data.decode().strip().split()
+                commands = recv_data.decode()
             except UnicodeDecodeError:
                 print("Decode Error")
                 continue
+            commands = apply_backspace(commands)
+            commands = commands.strip().split()
+            if not commands:
+                continue
+
             if commands[0] == "register":
                 if len(commands) != 4:
-                    self.request.sendall(HELP_REG)
-                    continue
-                _, username, email, password = commands
-                if client.find_one({"username": username}) is not None:
-                    self.request.sendall(FAIL_REG)
+                    self.wfile.write(HELP_REG)
                 else:
-                    client.insert_one(
-                        {"username": username, "email": email, "password": password})
+                    _, username, email, password = commands
+                    if collection.find_one({"username": username}) is None:
+                        collection.insert_one(
+                            {"username": username, "email": email, "password": password})
+                    else:
+                        self.wfile.write(FAIL_REG)
             elif commands[0] == "login":
                 if len(commands) != 3:
-                    self.request.sendall(HELP_LOGIN)
-                    continue
-                _, username, password = commands
-                if authorized:
-                    self.request.sendall(FAIL_LOGIN_ALREADY)
-                elif client.find_one({"username": username, "password": password}) is None:
-                    self.request.sendall(FAIL_LOGIN_INCORRECT)
+                    self.wfile.write(HELP_LOGIN)
+                elif name is None:
+                    _, username, password = commands
+                    if collection.find_one({"username": username, "password": password}) is None:
+                        self.wfile.write(FAIL_LOGIN_INCORRECT)
+                    else:
+                        name = username
+                        self.wfile.write(
+                            "Welcome, {}.\r\n".format(name).encode())
                 else:
-                    authorized = True
-                    name = username
-                    self.request.sendall(
-                        "Welcome, {}.\n".format(name).encode())
+                    self.wfile.write(FAIL_LOGIN_ALREADY)
+
             elif commands[0] == "logout":
-                if not authorized:
-                    self.request.sendall(FAIL_UNAUTHORIZED)
+                if name is None:
+                    self.wfile.write(FAIL_UNAUTHORIZED)
                 else:
-                    self.request.sendall("Bye, {}.\n".format(name).encode())
-                    authorized = False
+                    self.wfile.write("Bye, {}.\r\n".format(name).encode())
                     name = None
             elif commands[0] == "whoami":
-                if not authorized:
-                    self.request.sendall(FAIL_UNAUTHORIZED)
+                if name is None:
+                    self.wfile.write(FAIL_UNAUTHORIZED)
                 else:
-                    self.request.sendall("{}\n".format(name).encode())
+                    self.wfile.write("{}\r\n".format(name).encode())
 
             elif commands[0] == "exit":
-                print(self.client_address, " closed the connection.")
-                self.request.close()
+                print(self.client_address, "closed the connection.")
                 break
             else:
-                print(recv_data)
+                print("Unknown command:", commands)
+        self.request.shutdown(socket.SHUT_RDWR)
+        self.request.close()
 
 
-server = socketserver.ThreadingTCPServer(("", int(sys.argv[1])), Server)
-server.serve_forever()
+def main():
+    try:
+        port = int(sys.argv[1])
+    except IndexError:
+        port = 23
+    server = socketserver.ThreadingTCPServer(("0.0.0.0", port), Server)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
